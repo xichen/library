c6d37616404ec8feff9bf2a48ef6286775c01ab7
Xi Chen <xi.chen@eficode.fi>
2011-11-18 14:03:57 +0200

    first commit to initialize the repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = '313722ad4433540d82215f9077e1d59f1c150b9d9eb36115501334eb4f54558742490a79304c79ca790f19e9b436bf969674916ae4ff7bdd000b354d51c23d5d'
