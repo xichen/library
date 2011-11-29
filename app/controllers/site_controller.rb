101c1039560855df96aaf338d9688f4282b92ef1
Xi Chen <xi.chen@eficode.fi>
2011-11-29 18:17:42 +0200

    add isbn method

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,18 @@
 class SiteController < ApplicationController
   def index
   end
+  
+  def isbn
+    if request.post?
+      @isbn = params[:isbn]
+      if IsbnValidator.new.validate(@isbn)
+        flash[:notice] = "#{@isbn} is a valid ISBN!"
+        redirect_to isbn_validator_path 
+      else
+        flash.now[:error] = "#{@isbn} is not a valid ISBN!"
+        render :isbn
+      end
+    end
+  end
 
 end
