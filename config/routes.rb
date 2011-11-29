fa6083e8727c256260d221d27474bbe7fcb203be
Xi Chen <xi.chen@eficode.fi>
2011-11-29 18:18:56 +0200

    configure isbn_validator_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Library::Application.routes.draw do
    root :to => 'site#index'
+  
+   # ISBN Validator
+   match 'site/isbn' => 'site#isbn', :via => [:get, :post],
+                                     :as => :isbn_validator
 end
